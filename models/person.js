4a46728f6d4e679d0fb0c6fe564ec71ce8dfa28a
ellenra <ellen.rajala@gmail.com>
2023-10-15 14:39:16 +0300

    muutoksia

--- models/person.js
+++ models/person.js
@@ -1,10 +1,7 @@
 const mongoose = require('mongoose')
-
 mongoose.set('strictQuery', false)
 
 const url = process.env.MONGODB_URI
-
-mongoose.set('strictQuery', false)
 
 console.log('connecting to', url)
 
@@ -15,15 +12,6 @@ mongoose.connect(url)
   .catch((error) => {
     console.log('error connecting to MongoDB:', error.message)
   })
-
-if (process.argv.length<3) {
-  console.log('give password as argument')
-  process.exit(1)
-}
-
-const password = process.argv[2]
-const person_name = process.argv[3]
-const person_number = process.argv[4]
 
 const personSchema = new mongoose.Schema({
   name: String,
@@ -37,24 +25,5 @@ personSchema.set('toJSON', {
     delete returnedObject.__v
   }
 })
-
-const Person = mongoose.model('Person', personSchema)
-
-
-if(process.argv.length === 3) {
-    Person.find({}).then(result => {
-        console.log(`phonebook:`)
-        result.forEach(person => {
-            console.log(`${person.name} ${person.number}`)
-        })
-        mongoose.connection.close()
-    })
-} else if(process.argv.length === 5) {
-    person.save().then(result => {
-        console.log(`added ${person_name} number ${person_number} to phonebook`)
-        mongoose.connection.close()
-      })
-}
-
 
 module.exports = mongoose.model('Person', personSchema)
